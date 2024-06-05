d58c6c521a68df4ced5e3e83e008f626abcbc9d8
Dilip Patel <pateldilip2902@gmail.com>
2024-06-05 18:15:01 +0530

    added material cardview layout

---
+++ IPL/app/src/main/java/com/dilip/ipl/CricketTitle.kt
@@ -0,0 +1,23 @@
+package com.dilip.ipl
+
+data class CricketTitle(
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val descriptionLong: String = "",
+    val buttonText: String = "",
+    val headerImageResId: Int = 0,
+    val headerImageUrl: String? = null
+)
+
+// rcb = https://wallpapercave.com/wp/wp8792167.jpg
+//https://pbs.twimg.com/profile_banners/70931004/1711378033/1080x360
+// csk = https://pbs.twimg.com/profile_banners/117407834/1716728989/1080x360
+// dc = https://pbs.twimg.com/profile_banners/176888549/1711182263/1080x360
+// kkr - https://pbs.twimg.com/profile_banners/23592970/1716764122/1080x360
+// lsg = https://pbs.twimg.com/profile_banners/4824087681/1715061946/1080x360
+// mi = https://pbs.twimg.com/profile_banners/106345557/1711875159/1080x360
+// pbks = https://pbs.twimg.com/profile_banners/30631766/1716187642/1080x360
+// rr = https://pbs.twimg.com/profile_banners/17082958/1713879322/1080x360
+// srh = https://pbs.twimg.com/profile_banners/989137039/1709706880/1080x360
+// gt = https://pbs.twimg.com/profile_banners/1476438846988427265/1715923934/1080x360
