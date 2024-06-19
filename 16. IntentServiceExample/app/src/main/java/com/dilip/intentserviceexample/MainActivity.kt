71b72a8af3dfa11861a255f867748e8212badaa2
Dilip Patel <pateldilip2902@gmail.com>
2024-06-19 11:13:53 +0530

    Service Example

--- 16. IntentServiceExample/app/src/main/java/com/dilip/intentserviceexample/MainActivity.kt
+++ 16. IntentServiceExample/app/src/main/java/com/dilip/intentserviceexample/MainActivity.kt
@@ -2,28 +2,58 @@ package com.dilip.intentserviceexample
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.dilip.intentserviceexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //////////////////////// MyIntentService /////////////////////////////
+        /*  binding.btnStartSerivce.setOnClickListener {
+              Intent(this, MyIntentService::class.java).also {
+                  startService(it)
+                  binding.tvServiceInfo.text = "Service running"
+
+              }
+          }
+
+          binding.btnStopService.setOnClickListener {
+              MyIntentService.stopService()
+              binding.tvServiceInfo.text = "Service Stopped"
+          }
+          */
+
+
+        //////////////////////// MyService /////////////////////////////
         binding.btnStartSerivce.setOnClickListener {
-            Intent(this, MyIntentService::class.java).also {
+            Intent(this, MyService::class.java).also {
                 startService(it)
                 binding.tvServiceInfo.text = "Service running"
-
             }
         }
 
         binding.btnStopService.setOnClickListener {
-            MyIntentService.stopService()
-            binding.tvServiceInfo.text = "Service Stopped"
+            Intent(this, MyService::class.java).also {
+                stopService(it)
+                binding.tvServiceInfo.text = "Service Stopped"
+            }
+
         }
 
+        binding.btnSendData.setOnClickListener {
+            Intent(this, MyService::class.java).also {
+                val dataString = binding.etData.text.toString()
+                it.putExtra("EXTRA_DATA", dataString)
+                startService(it)
+            }
+        }
     }
+
+
 }
